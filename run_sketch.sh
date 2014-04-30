75028f1d5a1b8cf46c45c6d2875c2d3b126e26db
Joel Bernier <joelvbernier@me.com>
2014-04-30 15:07:58 -0700

    updated .gitignore, added run script

---
+++ run_sketch.sh
@@ -0,0 +1,5 @@
+cd /Users/bernier2/Documents/HEDM_geometry
+sketch -o /Users/bernier2/Documents/HEDM_geometry/new_geometry.sk.out /Users/bernier2/Documents/HEDM_geometry/new_geometry.sk
+latex /Users/bernier2/Documents/HEDM_geometry/new_geometry.tex
+dvips -o /Users/bernier2/Documents/HEDM_geometry/new_geometry.ps /Users/bernier2/Documents/HEDM_geometry/new_geometry.dvi
+dvipdf /Users/bernier2/Documents/HEDM_geometry/new_geometry.dvi /Users/bernier2/Documents/HEDM_geometry/new_geometry.pdf
